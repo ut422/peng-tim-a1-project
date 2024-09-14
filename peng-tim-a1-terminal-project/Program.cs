15f10439c5d8a7544d005b694120ef1e9dab9c59
h <mlgdashyt@gmail.com>
2024-09-13 22:45:42 -0400

    first start

--- peng-tim-a1-terminal-project/Program.cs
+++ peng-tim-a1-terminal-project/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DadJokes101
+{
+    class Program
+    {
+        static void Main()
+  
