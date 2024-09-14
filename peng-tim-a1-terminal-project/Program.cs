8742bd2a61ca5ce6ad4ebaae881dd6069f271d21
h <mlgdashyt@gmail.com>
2024-09-14 00:55:13 -0400

    halfway code

--- peng-tim-a1-terminal-project/Program.cs
+++ peng-tim-a1-terminal-project/Program.cs
@@ -5,4 +5,36 @@ namespace DadJokes101
     class Program
     {
         static void Main()
-  
+        {
+            // introduction
+            Console.WriteLine("dad jokes.");
+            Console.WriteLine("dad jokes? (yes/no)");
+
+            string response = Console.ReadLine();
+
+            // ready check
+            if (response == "yes")
+            {
+                Console.WriteLine("ahem-");
+
+                // first joke
+                Console.WriteLine("\nWhy don't skeletons fight each other?");
+                Console.WriteLine("do you know the answer? (yes/no)");
+
+                string answer = Console.ReadLine();
+
+                // joke punchline
+                if (answer == "yes")
+                {
+                    Console.WriteLine("'They don't have the guts!'");
+                }
+                else if (answer == "no")
+                {
+                    Console.WriteLine("here's the punchline anyways: 'They don't have the guts!'");
+                }
+                else
+                {
+                    Console.WriteLine("eh, who cares: 'They don't have the guts!'");
+                }
+
+              
